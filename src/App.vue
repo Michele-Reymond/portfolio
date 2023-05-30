4c11e6bf3d81d3b2f51f055c0578e8cf1a3beeb2
Michele-Reymond <hello@michelereymond.ch>
2023-05-30 17:19:32 +0200

    Title en cours

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,23 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Hello from './components/Hello.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+      
   </header>
 
   <main>
-    <TheWelcome />
+    <Hello/>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
+  main {
+    height: 100%;
+    width: 100%;
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    align-items: center;
+    justify-content: center;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,47 @@
+<script setup>
+</script>
+
+<template>
+    <h1>Hello! Moi c'est <a class="home-link" id="michele" href="#">Michèle</a>.<br> Je fais du <a class="home-link" id="motion" href="#">motion</a>, de <a id="illu" class="home-link" href="#">l'illu</a> et de la <a id="prog" class="home-link" href="#">prog</a>.</h1>
+</template>
+
+<style scoped>
+  h1  {
+    width: 70%;
+    font-family: Chillax;
+    font-size: 64px;
+    font-weight: 600;
+    line-height: 89px;
+    letter-spacing: 0.02em;
+    text-align: center;
+  }
+
+  .home-link {
+    text-decoration: none;
+    color: black;
+    transition: all .4s ease;
+  }
+
+  #michele:hover {
+    color: white;
+    text-decoration: underline;
+    text-decoration-thickness: 12px;
+  }
+
+  #motion:hover {
+    color: #7000FF;
+    text-decoration: wavy underline;
+    text-decoration-thickness: 8px;
+  }
+
+  #illu:hover {
+    background-color: #FB98DF;
+  }
+  
+  #prog:hover {
+    color: #FF7A00;
+    text-decoration: dotted underline;
+    text-decoration-thickness: 8px;
+    text-underline-offset: 18px;
+  }
+</style>
